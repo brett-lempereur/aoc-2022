a0838b2eb59695d49c5500f27891f661944f572b
Brett Lempereur <naufauna@gmail.com>
2022-12-01 23:50:13 +0000

    Refine the solution for day one

--- day-1/solution.rkt
+++ day-1/solution.rkt
@@ -6,14 +6,13 @@
 (define (file->calories filename)
   (define input-lines (in-lines (open-input-file filename)))
   (define input-numbers (sequence-map string->number input-lines))
-  (for/fold ([acc '(0)])
-            ([maybe-number input-numbers])
-    (if maybe-number
-        (cons (+ (car acc) maybe-number) (cdr acc))
-        (cons 0 acc))))
+  (for/fold ([acc '(0)]) ([maybe-number input-numbers])
+    (if maybe-number (cons (+ (car acc) maybe-number) (cdr acc)) (cons 0 acc))))
 
-(define (top-n-calories calories n) (take (sort calories >) n))
-(define (sum calories) (foldl + 0 calories))
+(define (top-n-calories calories n)
+  (take (sort calories >) n))
+(define (sum calories)
+  (foldl + 0 calories))
 
 ;; Compute the total calories held by the top and top three elves.
 (define calories-by-elf (file->calories "data/input.txt"))
