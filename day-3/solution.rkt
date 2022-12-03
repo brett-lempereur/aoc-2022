d1c6c9f20e64ff817d7fee75841e7767e98a193b
Brett Lempereur <naufauna@gmail.com>
2022-12-03 19:45:40 +0000

    Simplify the solution for day three

--- day-3/solution.rkt
+++ day-3/solution.rkt
@@ -12,24 +12,23 @@
 (define (input->backpacks filename)
   (define (line->backpack line)
     (map char->priority (string->list line)))
-  (sequence-map line->backpack (in-lines (open-input-file filename))))
+  (sequence->list
+   (sequence-map line->backpack (in-lines (open-input-file filename)))))
 
-(define (duplicate-item-priority backpack)
-  (define-values (l r) (split-at backpack (/ (length backpack) 2)))
-  (car (set->list (set-intersect (list->set l) (list->set r)))))
+(define (common-element sets)
+  (car (set->list (foldl set-intersect (car sets) (cdr sets)))))
 
-(define (badge-priority a b c)
-  (car (set->list (set-intersect (list->set a) (list->set b) (list->set c)))))
+(define (split-backpack backpack)
+  (define-values (a b) (split-at backpack (/ (length backpack) 2)))
+  (list (list->set a) (list->set b)))
 
-(define (badge-priorities backpacks)
-  (define-values (head tail) (split-at backpacks 3))
-  (define priority (apply badge-priority head))
-  (+ priority (if (empty? tail) 0 (badge-priorities tail))))
+(define (chunk backpacks n)
+  (define-values (a b) (split-at backpacks n))
+  (cons a (if (empty? b) '() (chunk b n))))
 
 (define (sum input)
   (sequence-fold + 0 input))
 
-(printf "Solution to part one: ~a\n"
-        (sum (sequence-map duplicate-item-priority (input->backpacks "data/input.txt"))))
-(printf "Solution to part two: ~a\n"
-        (badge-priorities (sequence->list (input->backpacks "data/input.txt"))))
+(define backpacks (input->backpacks "data/input.txt"))
+(printf "Solution one: ~a\n" (sum (map (compose common-element split-backpack) backpacks)))
+(printf "Solution two: ~a\n" (sum (map common-element (chunk backpacks 3))))
